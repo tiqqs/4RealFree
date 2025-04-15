b3a39ef50a1491bb153c759112692a0de95f6d05
tiqqs <emirslaimani@gmail.com>
2025-04-15 19:02:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "4RealFree - Free Games",
   description: "Discover the best free-to-play games for PC and browser",
-    generator: 'v0.dev'
+  //generator: 'v0.dev'
 }
 
 export default function RootLayout({
